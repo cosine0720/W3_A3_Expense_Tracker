c5f6fac22dd30322a698e21f260b810b6791e8ba
cosine0720 <cosine0720@gmail.com>
2022-11-04 17:50:19 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(PORT, () => {
+  console.log(`This app is opening on http://localhost:${PORT}`)
+})
