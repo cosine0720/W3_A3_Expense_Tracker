285ced274485fe0d8e664d8cb969037715ff864d
cosine0720 <cosine0720@gmail.com>
2022-11-08 16:14:32 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Record.find() // 取出 Record model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort({ _id: 'asc' }) //根據 _id 升冪排序
     .then(records => res.render('index', { records })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
