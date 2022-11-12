a54d00087c00d8cedc3306e4028d67c3113eba59
cosine0720 <cosine0720@gmail.com>
2022-11-13 00:00:45 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -16,6 +17,11 @@ const PORT = process.env.PORT || 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main', helpers: handlebarsHelpers }))
 app.set('view engine', 'handlebars')
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.static('public'))
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
