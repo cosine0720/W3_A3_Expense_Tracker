f4ca9de8bca2bb05a35517deee8a68fda6adaa6c
cosine0720 <cosine0720@gmail.com>
2022-11-13 21:26:24 +0800

    modify todoSeeder

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,16 +1,35 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const bcrypt = require('bcryptjs')
 // 載入 record model
 const Record = require('../record')
+const User = require('../user')
 const db = require('../../config/mongoose')
+
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
 
 // 連線成功
 db.once('open', () => {
-  console.log('recordSeeder: mongodb connected!')
-  Promise.all(
-    Array.from(
-      { length: 3 },
-      (_, i) => Record.create({ name: `帝寶${i}號別墅`, date: `2022-12-01`, category: 1, amount: 60 })
-    )
-  )
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: 3 },
+        (_, i) => Record.create({ name: `帝寶${i}號別墅`, date: `2022-12-01`, category: 1, amount: 60, userId })
+      ))
+    })
     .then(() => {
       console.log('----- recordSeeder: done 啊！------')
       process.exit()
